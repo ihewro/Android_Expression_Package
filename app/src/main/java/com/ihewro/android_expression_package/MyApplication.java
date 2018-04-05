cf41b516684237f9f558e8e892f93cc6b1952cd0
hewro <ihewro@163.com>
2018-04-05 19:58:24 +0800

    init

---
+++ app/src/main/java/com/ihewro/android_expression_package/MyApplication.java
@@ -0,0 +1,100 @@
+package com.ihewro.android_expression_package;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+
+import com.bilibili.magicasakura.utils.ThemeUtils;
+import com.ihewro.android_expression_package.util.ThemeHelper;
+
+
+/**
+ * <pre>
+ *     author : hewro
+ *     e-mail : ihewro@163.com
+ *     time   : 2018/04/05
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class MyApplication extends Application implements ThemeUtils.switchColor{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ThemeUtils.setSwitchColor(this);
+    }
+
+    @Override
+    public int replaceColorById(Context context, @ColorRes int colorId) {
+        if (ThemeHelper.isDefaultTheme(context)) {
+            return context.getResources().getColor(colorId);
+        }
+        String theme = getTheme(context);
+        if (theme != null) {
+            colorId = getThemeColorId(context, colorId, theme);
+        }
+        return context.getResources().getColor(colorId);
+    }
+
+    @Override
+    public int replaceColor(Context context, @ColorInt int originColor) {
+        if (ThemeHelper.isDefaultTheme(context)) {
+            return originColor;
+        }
+        String theme = getTheme(context);
+        int colorId = -1;
+
+        if (theme != null) {
+            colorId = getThemeColor(context, originColor, theme);
+        }
+        return colorId != -1 ? getResources().getColor(colorId) : originColor;
+    }
+
+    private String getTheme(Context context) {
+        if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_STORM) {
+            return "blue";
+        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_HOPE) {
+            return "purple";
+        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_WOOD) {
+            return "green";
+        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_LIGHT) {
+            return "green_light";
+        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_THUNDER) {
+            return "yellow";
+        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_SAND) {
+            return "orange";
+        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_FIREY) {
+            return "red";
+        }
+        return null;
+    }
+
+    private
+    @ColorRes
+    int getThemeColorId(Context context, int colorId, String theme) {
+        switch (colorId) {
+            case R.color.theme_color_primary:
+                return context.getResources().getIdentifier(theme, "color", getPackageName());
+            case R.color.theme_color_primary_dark:
+                return context.getResources().getIdentifier(theme + "_dark", "color", getPackageName());
+            case R.color.theme_color_primary_trans:
+                return context.getResources().getIdentifier(theme + "_trans", "color", getPackageName());
+        }
+        return colorId;
+    }
+
+    private
+    @ColorRes
+    int getThemeColor(Context context, int color, String theme) {
+        switch (color) {
+            case 0xfffb7299:
+                return context.getResources().getIdentifier(theme, "color", getPackageName());
+            case 0xffb85671:
+                return context.getResources().getIdentifier(theme + "_dark", "color", getPackageName());
+            case 0x99f0486c:
+                return context.getResources().getIdentifier(theme + "_trans", "color", getPackageName());
+        }
+        return -1;
+    }
+}
