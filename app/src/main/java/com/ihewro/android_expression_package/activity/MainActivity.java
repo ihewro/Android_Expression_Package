fca0e58b63076fb724beb3e047e382c10feaf207
hewro <ihewro@163.com>
2018-04-06 12:41:25 +0800

    状态栏完美解决问题

--- app/src/main/java/com/ihewro/android_expression_package/activity/MainActivity.java
+++ app/src/main/java/com/ihewro/android_expression_package/activity/MainActivity.java
@@ -1,29 +1,39 @@
 package com.ihewro.android_expression_package.activity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.WindowManager;
-import android.widget.ImageView;
+import android.view.ViewGroup;
 
 import com.ihewro.android_expression_package.R;
+import com.ihewro.android_expression_package.adapter.ViewPagerAdapter;
+import com.ihewro.android_expression_package.fragment.ExpressionContentFragment;
 import com.jaeger.library.StatusBarUtil;
 import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.SectionDrawerItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    //@BindView(R.id.searchEdit)
+    @BindView(R.id.searchEdit)
     SearchView searchEdit;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -31,8 +41,13 @@ public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     @BindView(R.id.viewPager)
     ViewPager viewPager;
+    @BindView(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout collapsingToolbar;
+    @BindView(R.id.main_item)
+    CoordinatorLayout mainItem;
 
     private Drawer result;
+    private AccountHeader headerResult;
 
 
     @Override
@@ -41,11 +56,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        StatusBarUtil.setTranslucentForImageView(this,100, toolbar); //全透明
+        setSupportActionBar(toolbar);
 
-        /*result = new DrawerBuilder()
+        headerResult = new AccountHeaderBuilder()
                 .withActivity(this)
-                //.withAccountHeader(headerResult)
+                .withCompactStyle(false)
+                .withHeaderBackground(R.drawable.bg)
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(headerResult)
                 .withToolbar(toolbar)
                 .withFullscreen(true)
                 .addDrawerItems(
@@ -57,8 +79,69 @@ public class MainActivity extends AppCompatActivity {
                         new SecondaryDrawerItem().withName("五星好评").withIcon(FontAwesome.Icon.faw_question).withEnabled(false)
                 )
                 .withSavedInstance(savedInstanceState)
-                .build();*/
+                .build();
+
+
+        initTabLayout();
+
+        StatusBarUtil.setTranslucentForImageViewInFragment(this, toolbar);
+        //StatusBarUtil.setTransparent(this); //透明
+        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) mainItem.getLayoutParams();
+        layoutParams.setMargins(layoutParams.leftMargin, -(getStatusBarHeight(this)),
+                layoutParams.rightMargin, layoutParams.bottomMargin);
 
     }
+
+
+    /**
+     * 获取状态栏高度
+     *
+     * @param context context
+     * @return 状态栏高度
+     */
+    private static int getStatusBarHeight(Context context) {
+        // 获得状态栏高度
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        return context.getResources().getDimensionPixelSize(resourceId);
+    }
+
+    /**
+     * 初始化TabLayout 数据
+     */
+    private void initTabLayout() {
+        setViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+    }
+
+    /**
+     * 设置ViewPager
+     */
+    private void setViewPager(ViewPager viewPager) {
+        //碎片列表
+        List<Fragment> fragmentList = new ArrayList<>();
+        ExpressionContentFragment currentAudioIntroFragment = new ExpressionContentFragment();
+        Bundle bundle = new Bundle();
+
+
+        currentAudioIntroFragment.setArguments(bundle);
+        fragmentList.add(currentAudioIntroFragment);
+        fragmentList.add(new ExpressionContentFragment());
+
+
+        //标题列表
+        List<String> pageTitleList = new ArrayList<>();
+        pageTitleList.add("坏坏");
+        pageTitleList.add("猥琐萌");
+
+
+        //新建适配器
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), fragmentList, pageTitleList);
+
+        //设置ViewPager
+        viewPager.setAdapter(adapter);
+    }
+
 
 }
---
+++ app/src/main/java/com/ihewro/android_expression_package/adapter/ViewPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.ihewro.android_expression_package.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * <pre>
+ *     author : hewro
+ *     e-mail : ihewro@163.com
+ *     time   : 2018/04/06
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    List<Fragment> fragmentList;
+    List<String> pageTitleList;
+
+    public ViewPagerAdapter(FragmentManager fm, List<Fragment> fragmentList, List<String> pageTitleList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+        this.pageTitleList = pageTitleList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return pageTitleList.get(position);
+    }
+}
+
